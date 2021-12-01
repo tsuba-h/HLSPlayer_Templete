90a15a9baf9c0222c7e20d0b07aa5238a8615254
服部翼 <tsuba-h@arsaga.jp>
2021-12-01 19:02:34 +0900

    no message

--- HLS_TempleteTests/HLS_TempleteTests.swift
+++ HLS_TempleteTests/HLS_TempleteTests.swift
@@ -10,6 +10,10 @@ import XCTest
 
 class HLS_TempleteTests: XCTestCase {
 
+    /// テストの開始時に最初に一度呼ばれる関数。テストケースを回すために必要な設定やインスタンスの生成などを行う。
+    override func setUp() {
+    }
+
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
@@ -23,6 +27,7 @@ class HLS_TempleteTests: XCTestCase {
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
 
+    /// パフォーマンスの計測用の関数です。 self.measure {} のクロージャの中に計測したい処理を記述するとその処理にかかった時間を教えてくれます。
     func testPerformanceExample() throws {
         // This is an example of a performance test case.
         self.measure {
